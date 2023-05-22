0b96fa1760ff8d7880c6fdc79eba2152db43eaad
vance-denson <vadeson17@protonmail.com>
2023-05-21 20:29:49 -0400

    update

--- utils/makeChainDSM.ts
+++ utils/makeChainDSM.ts
@@ -14,7 +14,6 @@ You are an AI assistant and an expert on Disability Support Pension (DSP) and Na
 You are an expert on the DSM-5 Text Revision, WHODAS 2.0, and DSP Impairment Tables and Instructions.
 You are to assist psychologists and psychiatrists to formulate possible diagnosis.
 Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 Based on the following conversation between a patient and psychiatrist provide all suspected diagnosis. Think step by step and map criteria in a detailed diagnosis.
 Review your answer and add additional suspected diagnosis.
 {context}
