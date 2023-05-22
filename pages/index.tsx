9936b83713acf8c25ba21e9835d81afc3f594d92
vance-denson <vadeson17@protonmail.com>
2023-05-21 20:26:50 -0400

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,10 +53,7 @@ export const ChatDSM = ({ onClick }) => {
     }>({
         messages: [
             {
-                message: `Hello, welcome to chatDSM! Ask any question about the DSM-5 and WHODAS 2.0. If you would like me to diagnose a conversation please type\n:
-                "Based on the following conversation between a patient and psychiatrist provide all suspected diagnosis.
-                Think step by step and map criteria in a detailed diagnosis.
-                Review your answer and add additional suspected diagnosis." \nfollowed by the conversation.`,
+                message: 'Welcome! Enter your patient conversation below for suggested diagnosis with references:',
                 type: 'apiMessage',
             },
         ],
--- utils/makeChainDSM.ts
+++ utils/makeChainDSM.ts
@@ -9,7 +9,14 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are an AI assistant. You are to assist psychologists and psychiatrists to formulate a possible diagnosis and provide helpful suggestion and hypothesis.
+const QA_PROMPT = `If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+You are an AI assistant and an expert on Disability Support Pension (DSP) and National Disability Insurance Scheme (NDIS).
+You are an expert on the DSM-5 Text Revision, WHODAS 2.0, and DSP Impairment Tables and Instructions.
+You are to assist psychologists and psychiatrists to formulate possible diagnosis.
+Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+Based on the following conversation between a patient and psychiatrist provide all suspected diagnosis. Think step by step and map criteria in a detailed diagnosis.
+Review your answer and add additional suspected diagnosis.
 {context}
 Question: {question}
 Helpful answer in markdown:`;
