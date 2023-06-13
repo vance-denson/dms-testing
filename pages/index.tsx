77d2ecff86bd585ef8d4e72eca65222b62db2de9
vance-denson <vadeson17@protonmail.com>
2023-06-13 00:51:01 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ export const ChatDSM = ({ onClick }) => {
     }>({
         messages: [
             {
-                message: 'Welcome. Ask any question, or say "diagnose the following converstaion:" and paste the doctor-patient conversation!',
+                message: 'Welcome. Ask any question related to the DSM–5-TR or WHODAS 2.0, or say "diagnose the following conversation: <paste conversation>" and paste the doctor-patient conversation!',
                 type: 'apiMessage',
             },
         ],
